8c95ee3683e1146a21325f9de5f4875c43678a1c
TanmayJain33 <tanmay.jain@crownstack.com>
2022-01-31 17:12:24 +0530

    Add configurations

--- source/firebase/config.js
+++ source/firebase/config.js
@@ -1,10 +1,10 @@
 import Firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
-  apiKey: '',
-  databaseURL: '',
-  projectId: '',
-  appId: '',
+  apiKey: 'AIzaSyB8itcYC0f4x_9dGRfLTWptOQX4KKMBvuA',
+  databaseURL: 'https://chatapp-b82da-default-rtdb.firebaseio.com/',
+  projectId: 'chatapp-b82da',
+  appId: '1:738477874856:android:cf2aa224cfe8d412b82c9d',
 };
 
 export default Firebase.initializeApp(firebaseConfig);
